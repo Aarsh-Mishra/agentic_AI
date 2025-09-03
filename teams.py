312bf2d2351cd57d22858bdbaf5262ca387be435
Aarsh <aarshm21@gmail.com>
2025-09-03 16:57:31 +0530

    Added termination conditions for the task

--- teams.py
+++ teams.py
@@ -4,6 +4,8 @@ from autogen_agentchat.teams import RoundRobinGroupChat
 from autogen_agentchat.agents import (AssistantAgent)
 from autogen_core.models import ModelInfo
 from autogen_agentchat.messages import TextMessage
+from autogen_agentchat.conditions import MaxMessageTermination,TextMentionTermination
+from autogen_agentchat.base import TaskResult
 from dotenv import load_dotenv
 import os
 
@@ -40,25 +42,39 @@ async def teams():
     ending_agent = AssistantAgent(
         name = 'ending_writer',
         model_client=model_client,
-        system_message="You wrute engaging endings. conclude the story with a twist."
+        system_message="You write engaging endings. conclude the story with a twist."
     )
 
-    
+    review_agent = AssistantAgent(
+        name = 'review_writer',
+        model_client=model_client,
+        system_message="You have to say 'APPROVE' once the story is complete and you like the full flow else provide your feedback."
+    )
+
+    termination_condition = MaxMessageTermination(10) | TextMentionTermination('APPROVE')
 
     team = RoundRobinGroupChat(
-        participants= [plot_agent, character_agent, ending_agent],
-        max_turns=6
+        participants= [plot_agent, character_agent, ending_agent, review_agent],
+        termination_condition=termination_condition
+        # max_turns=6
     )
 
     async def test_team():
         task = TextMessage(
-            content='Write a short story a brave boy and his Pokemon. Keep it up to 50 words',
+            content='Write a short story a brave boy and his Pokemon. Keep it up to 20 words',
             source='user'
         )
 
         result = await team.run(task=task)
         for each_agent_message in result.messages:
             print(f'{each_agent_message.source} : {each_agent_message.content}')
+        
+        await team.reset() 
+        async for message in team.run_stream(task="Write a short poem about the summer season in India."):  
+            if isinstance(message, TaskResult):
+                print("Stop Reason:", message.stop_reason)
+            else:
+                print(message)
         
 
     await test_team()
